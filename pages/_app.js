c0206f082d1ea21e2bf74ef21446751c73009b28
applePie0072 <apple.star@gmail.com>
2021-11-26 20:28:32 -0300

    style change

--- pages/_app.js
+++ pages/_app.js
@@ -4,33 +4,36 @@ import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
 	  return (
-		      <div>
+		      <div className="bg-black">
 		        <nav className="border-b p-6">
-		          <p className="text-4xl font-bold">SQUIRT NFT Marketplace - Connect to BSC via Metamask</p>
-		          <div className="flex mt-4">
+				  <div className="flex flex-row justify-left">
+					<div><img src="squirt-game-logo.png" alt="logo" width="200" height="100" /></div>
+					<div><p className="text-4xl font-bold text-white p-6 thisText" style={{fontFamily:'gotham rounded,sans-serif'}}>SQUIRT NFT Marketplace </p></div>
+		          </div>
+				  <div className="flex mt-4"  style={{fontFamily:'gotham rounded,sans-serif'}}>
 		            <Link href="/">
-		              <a className="mr-4 text-pink-500">
+		              <a className="mr-4 text-white thisText" >
 		                Home
 		              </a>
 		            </Link>
 		            <Link href="/create-item">
-		              <a className="mr-6 text-pink-500">
+		              <a className="mr-6 text-white thisText">
 		                Create New NFT
 		              </a>
 		            </Link>
 		            <Link href="/my-assets">
-		              <a className="mr-6 text-pink-500">
+		              <a className="mr-6 text-white thisText">
 		                Purchased NFT Assets
 		              </a>
 		            </Link>
 		            <Link href="/creator-dashboard">
-		              <a className="mr-6 text-pink-500">
+		              <a className="mr-6 text-white thisText">
 		                Creator Dashboard
 		              </a>
 		            </Link>
 		          </div>
 		        </nav>
-		        <Component {...pageProps} />
+		        <Component  className="bg-black" {...pageProps} />
 		      </div>
 		    )
 }
--- pages/create-item.js
+++ pages/create-item.js
@@ -76,8 +76,8 @@ export default function CreateItem() {
 		    }
 
 	  return (
-		      <div className="flex justify-center">
-		        <div className="w-1/2 flex flex-col pb-12">
+		      <div className="flex justify-center bg-black h-screen">
+		        <div className="w-1/2 flex flex-col pb-12" style={{fontFamily:'gotham rounded,sans-serif'}}>
 		          <input 
 		            placeholder="Asset Name"
 		            className="mt-8 border rounded p-4"
